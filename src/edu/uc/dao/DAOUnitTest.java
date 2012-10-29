3c0a69cc9ad229eaefd92204fd7cf87ddde6e9ee
iamclassic <iamclassic@localhost>
2012-10-29 17:28:50 -0400

    DAO Unit test

---
+++ src/edu/uc/dao/DAOUnitTest.java
@@ -0,0 +1,40 @@
+package edu.uc.dao;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAOUnitTest {
+
+	@Test
+	
+//Database tests will fail right now, still implementing. 
+	public void testonCreate() 
+	{
+		fail("Not yet implemented");	}
+
+	public void testsaveCoordinatesdatabase()
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testonUpgrade(){
+		fail("Not yet implemented");
+	}
+
+	//
+	public void testpersistCoordinates(){
+		fail("Not yet implemented");
+
+	}
+	public void testfetchDistanceObject(){
+		fail("Not yet implemented");
+
+	}
+	public void testpersistDistanceObject(){
+		fail("Not yet implemented");
+
+	}
+}
