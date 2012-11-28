a455ac2de958395aac8b89793f2fc8a8fc36f774
Eli <Eli@localhost>
2012-11-28 12:28:37 -0500

    Integrating Google Map in the project

--- gen/edu/uc/ui/R.java
+++ gen/edu/uc/ui/R.java
@@ -17,35 +17,38 @@ public final class R {
         public static final int timewidth=0x7f050000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int androidmaker=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int FrameLayout1=0x7f090005;
-        public static final int RelativeLayout1=0x7f090007;
-        public static final int btnskiptutorial=0x7f090006;
-        public static final int btnstartgpstracking=0x7f09000d;
-        public static final int btnstoptracking=0x7f090002;
-        public static final int btnviewresults=0x7f09000c;
-        public static final int chronometer1=0x7f09000b;
-        public static final int lblcurrentlytracking=0x7f090008;
-        public static final int lblgpscoordinate1=0x7f090003;
-        public static final int lblgpscoordinate2=0x7f090004;
-        public static final int lbllat=0x7f09000e;
-        public static final int lbllong=0x7f09000f;
-        public static final int lblstarttime=0x7f09000a;
-        public static final int lblstoptime=0x7f090009;
-        public static final int lblstoptracking=0x7f090010;
-        public static final int listView=0x7f090001;
-        public static final int menu_settings=0x7f090011;
-        public static final int textView1=0x7f090000;
+        public static final int FrameLayout1=0x7f090006;
+        public static final int RelativeLayout1=0x7f090008;
+        public static final int btnskiptutorial=0x7f090007;
+        public static final int btnstartgpstracking=0x7f09000e;
+        public static final int btnstoptracking=0x7f090003;
+        public static final int btnviewresults=0x7f09000d;
+        public static final int chronometer1=0x7f09000c;
+        public static final int lblcurrentlytracking=0x7f090009;
+        public static final int lblgpscoordinate1=0x7f090004;
+        public static final int lblgpscoordinate2=0x7f090005;
+        public static final int lbllat=0x7f09000f;
+        public static final int lbllong=0x7f090010;
+        public static final int lblstarttime=0x7f09000b;
+        public static final int lblstoptime=0x7f09000a;
+        public static final int lblstoptracking=0x7f090011;
+        public static final int listView=0x7f090002;
+        public static final int mapview=0x7f090000;
+        public static final int menu_settings=0x7f090012;
+        public static final int textView1=0x7f090001;
     }
     public static final class layout {
-        public static final int activity_results=0x7f030000;
-        public static final int activity_stop_tracking_gps=0x7f030001;
-        public static final int activity_tracking_gps=0x7f030002;
-        public static final int splashactivity=0x7f030003;
-        public static final int startgpsactivity=0x7f030004;
+        public static final int activity_map=0x7f030000;
+        public static final int activity_results=0x7f030001;
+        public static final int activity_stop_tracking_gps=0x7f030002;
+        public static final int activity_tracking_gps=0x7f030003;
+        public static final int splashactivity=0x7f030004;
+        public static final int startgpsactivity=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
