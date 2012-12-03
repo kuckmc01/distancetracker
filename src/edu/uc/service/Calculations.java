1ca3b6929774a612c76381e3009298d1246ca9d9
iamclassic <iamclassic@localhost>
2012-12-03 00:12:28 -0500

    Resolved calculations and added another for time

--- src/edu/uc/service/Calculations.java
+++ src/edu/uc/service/Calculations.java
@@ -1,6 +1,9 @@
 package edu.uc.service;
 
 import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.database.Cursor;
@@ -19,6 +22,46 @@ import android.widget.Toast;
 public  class Calculations  {
 	
 	static double  distancetotal;
+	
+	public static void time(GPSDAODatabase dao)
+	
+	{
+		Cursor myCursor = dao.Cursor(); 
+		String time1;
+		String time2;
+		myCursor.moveToFirst();
+		time1 = myCursor.getString(4);
+		myCursor.moveToLast();
+		time2 = myCursor.getString(4);
+		
+
+		DateFormat formatter;
+		
+		Date date1;
+		Date date2;
+		formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+		try {
+			date1 = (Date)formatter.parse(time1);
+			date2 = (Date)formatter.parse(time2);
+		
+			long milliseconds1 = date1.getTime();
+			System.out.println("milli1" + milliseconds1);
+			long milliseconds2= date2.getTime();
+
+			System.out.println("milli2" + milliseconds2);
+			float  totaltime = (milliseconds2 - milliseconds1) / 3600000;
+			System.out.println("The total time was:" + totaltime);
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	
+	}
+
 	public static void calc(GPSDAODatabase dao)
 	{
 		
@@ -27,19 +70,19 @@ public  class Calculations  {
 		double longitude1;
 		double latitude2;
 		double longitude2;
-		myCursor.moveToLast();
-		 String time2 = myCursor.getString(5);	
+		
 		myCursor.moveToFirst();
 		 latitude1 = Double.valueOf(myCursor.getString(2)) ;
-		 longitude1 = Double.valueOf(myCursor.getString(1)) ;
-		 String time1 = myCursor.getString(5);
+		 longitude1 = Double.valueOf(myCursor.getString(3)) ;
 		 
+			
+
 		 double totaltotal = 0 ;
 		while (!myCursor.isAfterLast())
 		{
 			
 			 latitude2 = Double.valueOf(myCursor.getString(2));
-			 longitude2 = Double.valueOf(myCursor.getString(1));
+			 longitude2 = Double.valueOf(myCursor.getString(3));
 			 
 			double totallat =  69.1 * (latitude2 - latitude1);
 			 double totallong = 69.1* (longitude2 - longitude1) * Math.cos(latitude1/57.3);
@@ -51,7 +94,10 @@ public  class Calculations  {
 			 distancetotal = Math.sqrt ((totallat * totallat) + (totallong * totallong));
 			
 			 totaltotal = distancetotal + totaltotal;
+			 
+				System.out.println("The subtotal distance is " + distancetotal);
 		}	 
+		System.out.println("The total distance is " + totaltotal);
 		}
 		
 	}
