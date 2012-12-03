5fd821f595d7964fdf23ec41cde303d1d3aea293
amaraeo309 <amaraeo309@localhost>
2012-12-03 12:07:16 -0500

    Just some clean up and added comments on the Map class and the ItemizedOverlay class.

--- src/edu/uc/ui/DistanceTrackerItemizedOverlay.java
+++ src/edu/uc/ui/DistanceTrackerItemizedOverlay.java
@@ -11,40 +11,48 @@ import android.graphics.drawable.Drawable;
 
 
 public class DistanceTrackerItemizedOverlay extends ItemizedOverlay {
-	
+
+	// create an OverlayItem ArrayList, in which you'll put each of the OverlayItem objects
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
-    
+
+	//This constructor defines the default marker for each of the OverlayItem objects
 	public DistanceTrackerItemizedOverlay(Drawable defaultMarker) {
-		  super(boundCenterBottom(defaultMarker));
-		}
-	public DistanceTrackerItemizedOverlay(Drawable defaultMarker, Context context) {
-		  super(boundCenterBottom(defaultMarker));
-		  mContext = context;
-		}
-	
-	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+		super(boundCenterBottom(defaultMarker));
 	}
-	
+
+	//This constructor set the ability to handle touch events on the overlay items.
+	public DistanceTrackerItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+
+	//this method will unable you to add a new OverlayItem objects to the ArrayList
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	//override this method to properly read from the ArrayList and return the OverlayItem from the position specified by the given integer.
 	@Override
 	protected OverlayItem createItem(int i) {
-	  return mOverlays.get(i);
+		return mOverlays.get(i);
 	}
-	
+
+	//Override this method to return the current number of items in the arrayList
 	@Override
 	public int size() {
-	  return mOverlays.size();
+		return mOverlays.size();
 	}
-	
+
+	//Override onTap method to handle the event when the user tapped on an item.
 	@Override
 	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
-	  dialog.show();
-	  return true;
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
 	}
 }
