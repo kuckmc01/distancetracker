c096854bf62efb7e0b1fcbaf68e03be0be55ff25
Eli <Eli@localhost>
2012-12-03 17:43:17 -0500

    Added map controller

--- src/edu/uc/ui/DistanceTrackerMap.java
+++ src/edu/uc/ui/DistanceTrackerMap.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
@@ -18,7 +19,7 @@ public class DistanceTrackerMap extends MapActivity {
 
 
 	private GPSDAODatabase dao;
-
+    private MapController mc;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,6 +31,7 @@ public class DistanceTrackerMap extends MapActivity {
 		mapView.setBuiltInZoomControls(true);
 		//initialize the dao instance.
 		dao = new GPSDAODatabase(this);
+		mc = mapView.getController();
 		List<Overlay> mapOverlays = mapView.getOverlays();
 		// add a picture to show where is your current position on the map
 		Drawable drawable = this.getResources().getDrawable(R.drawable.androidmaker);
@@ -37,17 +39,23 @@ public class DistanceTrackerMap extends MapActivity {
 		// get the coordinates from the database
 		List<Coordinates> points = dao.CursorForMap();
 		// loop through and display the coordinates on the map 
-		for (int i = 0; i < points.size();i++)
-		{
-			int testlat =  (int) dao.CursorForMap().get(i).getLatitude();
-			int testlong =  (int) dao.CursorForMap().get(i).getLongitude();
+		//for (int i = 0; i < points.size();i++)
+		//{
+			Double testlat =   dao.CursorForMap().get(0).getLatitude();
+			Double testlong =   dao.CursorForMap().get(0).getLongitude();
+			int geoLat = (int) (testlat.intValue() * 1E6);
+			int geoLong = (int) (testlong.intValue() * 1E6);
+					
 			//Create the GeoPoint that defines the map coordinates for the overlay item.
-			GeoPoint point = new GeoPoint(testlat, testlong);
+			GeoPoint point = new GeoPoint(geoLat, geoLong);
 			OverlayItem overlayitem = new OverlayItem(point, "" + testlat, "" + testlong);
+			
 			//add the overlay item to the collection and add the itemizedOverlay to the mapView
 			itemizedoverlay.addOverlay(overlayitem);
 			mapOverlays.add(itemizedoverlay);
-		} 
+			mc.animateTo(point);
+			mapView.invalidate();
+		//} 
 
 	}
 
